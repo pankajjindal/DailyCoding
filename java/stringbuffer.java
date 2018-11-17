5885c4725114d33f570b9ae9035b0d31963d7a1c
Pankaj Jindal <pankajjindal@Pankajs-MacBook-Air.local>
2018-11-17 08:35:03 -0800

    generics

--- java/stringbuffer.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-
-   public static void main(String args[]) {
-      StringBuffer sBuffer = new StringBuffer("test");
-      sBuffer.append(" String Buffer");
-      System.out.println(sBuffer);  
-   }
-}
