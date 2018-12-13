0813963b841f541649ac7e373648b41c46795b49
Pankaj Jindal <pankaj.jindal@bluerivert.com>
2018-12-13 14:25:47 -0800

    test

--- java/generics4.java
+++ java/generics4.java
@@ -2,7 +2,7 @@
 // generics can cause run time exceptions 
 import java.util.*; 
   
-class Test 
+class generics4 
 { 
     public static void main(String[] args) 
     { 
@@ -11,12 +11,23 @@ class Test
   
         al.add("Sachin"); 
         al.add("Rahul"); 
-        al.add(10); // Compiler allows this 
+        al.add(10); // Compiler allows this called autoboxing 
   
         String s1 = (String)al.get(0); 
         String s2 = (String)al.get(1); 
-  
-        // Causes Runtime Exception 
-        String s3 = (String)al.get(2); 
+ 
+        System.out.println(s1); 
+        System.out.println(s2); 
+
+        
+        //String s3 = (String)al.get(2); // RE:ClassCastException
+ 
+        //int i = al.get(2); //RE:Object cannot be converted to int
+
+        //Integer i = Integer.intValue(al.get(2)); //RE:Object cannot be converted to int
+        
     } 
 } 
+
+//we have no suitable way to get the value of al(2) since we do not know if its a int
+//this is where we generics fail so we need to be precise in defining.(correct this of needed)
