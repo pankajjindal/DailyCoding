b941c688b63ea8b3b85f031c09158577f70ed969
Pankaj Jindal <jindal25@gmail.com>
2019-05-25 15:56:17 -0700

    test3

---
+++ recursion/countPaths3.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+#define len 3
+
+int countPath(int **grid , int r , int c, int totalRow ,int totalCol );
+
+int MainCountPath(int **grid, int totalRow, int totalCol)
+{
+	return countPath(grid , 0 ,0, totalRow, totalCol);
+}
+
+int countPath(int **grid , int r , int c, int totalRow ,int totalCol )
+{
+
+	if (r>=totalRow-1 || c >=totalCol-1)
+		return 1;
+	printf ( " %d , %d\n", r ,c);
+
+	return (countPath( grid, r+1, c, totalRow, totalCol) + countPath(grid , r, c+1, totalRow, totalCol));
+	
+}
+
+int main()
+{
+	int grid[len][len] ;
+
+	for(int i=0;i<len;i++)
+        {
+            for(int j=0;j<len;j++)
+            {
+	           //printf("element - [%d],[%d] : ",i,j);
+               grid[i][j] = 1;
+               if (i==2 && j==3){
+               	   grid[i][j] ='*';
+               	   continue;
+               	}
+            }
+        }  
+
+        printf ( " The paths are %d \n" , MainCountPath(grid, len,len));
+}
+
