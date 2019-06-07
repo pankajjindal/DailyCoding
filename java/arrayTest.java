93fd76aac457df0f1c2068b8a14a40d79edc03f0
Pankaj Jindal <jindal25@gmail.com>
2019-06-07 00:29:46 -0700

    test

---
+++ java/arrayTest.java
@@ -0,0 +1,19 @@
+class arrayTest{
+   public static void main(String[] srgs)
+	{
+           int[][] x = new int[3][2];
+           int[][][] y=new int[2][][];
+           System.out.println(x);
+           System.out.println(x[0]);
+           System.out.println(x[0][0]);
+           
+                    y[0]= new int[3][];
+                    y[1]= new int [2][2];
+                   
+                    y[0][0] = new int[1];
+                    y[0][1] = new int[2];
+                    y[0][2] = new int[3];
+                    
+                    
+        }
+}
---
+++ java/testchar.java
@@ -0,0 +1,12 @@
+class testchar
+{
+	public static void main(String args[])
+	{
+ 		char ch =65536;
+                System.out.println(ch);
+ 		char ch1 =13;
+                System.out.println(ch1);
+ 		char ch2 =14;
+                System.out.println(ch2);
+	}
+}
