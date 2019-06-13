dfe957df96d64d686e477301c24f370ec09a6678
Pankaj Jindal <jindal25@gmail.com>
2019-06-13 11:33:40 -0700

    add

--- recursion/bracket.c
+++ recursion/bracket.c
@@ -1,11 +1,39 @@
 #include<stdio.h>
 
-bracket()
+
+void addtoString( char * str, char r)
+{
+	static int i = 0;
+
+    str[i]=r;
+
+}
+
+void bracketMain( int n)
+{
+	char * str = malloc ( 2*n);
+	bracket( str ,n, 0, 0)
+
+}
+
+void brackets( char * str, int n , int open , int close, )
 {
 
-	
-	brackets ( addtoString('('), open+1, close);
+	if(open + close == 2*n)
+		print( "%s" , str);
 
-	brackets(addtoString(')', open , close+1);
+	if (open < n)
+	    brackets (addtoString(str, '('), open+1, close, );
+
+	if(open > close)
+	    brackets(addtoString(str, ')'), open , close+1, );
+
+}
+
+
+int main()
+{
+
+	bracketMain(3);
 
 }
