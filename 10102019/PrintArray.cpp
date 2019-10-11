08b3d49147ebe39048a5c61e887141657c30c47c
pankaj <pankaj@fnupankaj.local>
2019-10-11 13:26:20 -0700

    1st change

--- 10102019/PrintArray.cpp
+++ 10102019/PrintArray.cpp
@@ -1,50 +1,62 @@
 #include <iostream>
 #include <stdio.h>
 
-int FindTheLength(char *arr)
-{
-	int count =0;
-	while(arr[count]!='\0'){
-    count++;
-	}
-	return count+1;
-}
-void printArray(char * arr)
-{
-	int Length = FindTheLength(arr);
-	for ( int i =0 ;i< Length ;i++)
-		printf ( "%c",arr[i]);
-
-	printf("\n");
-	
-}
-
-void swap( char * array, int i , int j)
-{
-	char temp = array[i];
-	array[i]=array[j];
-	array[j]=temp;
-
-}
+//#include <>
 
 
-void PermuteArray( char * arr, int i)
-{
-	int n = 3;//size of array
-    printArray( arr);
-	for (int j =i;j<n;j++)
-	{
-		swap(arr,j,i);
-		PermuteArray(arr,i+1);
-		swap(arr,i,j);
-	}
-}
+class Permute{
+
+	public:
+
+		int FindTheLength(char *arr) const
+		{
+			int count =0;
+			while(arr[count++]!='\0'){
+				//printf("%c \n", arr[count]);
+			}
+			printf(" count is %d\n", count);
+			return count-1;
+		}
+
+		void printArray(char * arr) const
+		{
+			//int Length = FindTheLength(arr);
+			for ( int i =0 ;i< 3 ;i++)
+				printf ( "%c",arr[i]);
+
+			printf("\n");
+
+		}
+
+		void swap( char * array, int i , int j) const
+		{
+			char temp = array[i];
+			array[i]=array[j];
+			array[j]=temp;
+
+		}
+
+		void PermuteArray( char * arr, int i) const
+		{
+			const int n = 3;//size of array
+			printArray( arr);
+			for (int j =i;j<n;j++)
+			{
+				swap(arr,j,i);
+				PermuteArray(arr,i+1);
+				swap(arr,i,j);
+			}
+		}
+
+};
 
 int main()
 {
+	Permute *per = new Permute();
+
 	char test[3]={'b','a','t'};
 
-	PermuteArray(test,0);
+	per->PermuteArray(test,0);
 
 	return 0;
 }
